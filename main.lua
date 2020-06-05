8c3c03531c7f39daab9a321c13342e41b0e1058f
Eddie <alexwerewolf2009@gmail.com>
2020-06-05 00:24:19 -0400

    refactor code.

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 BUTTON_HEIGHT = 64 
+WINDOW_WIDTH = love.graphics.getWidth() 
+WINDOW_HEIGHT = love.graphics.getHeight() 
 
 local 
 function newButton(text,fn)
@@ -6,12 +8,17 @@ function newButton(text,fn)
         text = text, 
         fn = fn, 
         now = false, 
-        last = false
+        last = false, 
+        baseColor = {0.4,0.4,0.5, 1.0},
+        hotColor = {0.8,0.8,0.9,1.0},
+        currentColor = {}
     }
 end 
 
 local buttons = {} 
 local font = nil  
+
+
 function love.load()
     font = love.graphics.newFont(32)
     table.insert(buttons, newButton("Start Game", function() print("Starting Gameing") end))
@@ -25,50 +32,34 @@ function love.update(dt)
 end 
 
 function love.draw()
-    local ww = love.graphics.getWidth() 
-    local wh = love.graphics.getHeight() 
-
-    local button_width = ww * (1/3)
+    local BUTTON_WIDTH = WINDOW_WIDTH * (1/3)
     local margin = 16
     local total_height = (BUTTON_HEIGHT + margin) * #buttons
-    local cursor_y = 0;
-
 
     for i, button in ipairs(buttons) do 
         button.last = button.now 
-        local x = (ww * 0.5) - (button_width * 0.5) 
-        local y = (wh * 0.5) - (total_height * 0.5) + cursor_y
-        local color = {0.4,0.4,0.5, 1.0}
-    
+
+        local x = (WINDOW_WIDTH/2) - (BUTTON_WIDTH/2) 
+        local y = (WINDOW_HEIGHT/2) - (total_height/2) + ((BUTTON_HEIGHT + margin) * i)
+
         local mx, my = love.mouse.getPosition() 
+        local hot = mx > x and mx < x + BUTTON_WIDTH and my > y and my < y + BUTTON_HEIGHT
     
-        local hot = mx > x and mx < x + button_width and 
-                    my > y and my < y + BUTTON_HEIGHT
-    
-        if hot then 
-            color = {0.8,0.8,0.9,1.0}
-        end 
+        button.currentColor = button.baseColor
+
+        if hot then button.currentColor = button.hotColor end 
         
         button.now = love.mouse.isDown(1)
 
-        if button.now and not button.last and hot then 
-            button.fn()
-        end 
+        if button.now and not button.last and hot then button.fn() end 
 
-        love.graphics.setColor(unpack(color))
-        love.graphics.rectangle("fill", 
-        x, 
-        y,
-        button_width, 
-        BUTTON_HEIGHT)    
-
+        love.graphics.setColor(unpack(button.currentColor))
+        love.graphics.rectangle("fill", x, y, BUTTON_WIDTH, BUTTON_HEIGHT)
         love.graphics.setColor(0,0,0,1)
 
         local textW = font:getWidth(button.text)
         local textH = font:getHeight(button.text)
-        love.graphics.print(button.text, font,(ww * 0.5) - textW * 0.5,(y + textH * 0.5 ) )
-
-        cursor_y = cursor_y + (BUTTON_HEIGHT + margin)
+        love.graphics.print(button.text, font,(WINDOW_WIDTH * 0.5) - textW * 0.5,(y + textH * 0.5 ) )
 
      end 
 
