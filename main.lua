38461171b670af3cb7661d6c8704699549ab3968
Eddie <alexwerewolf2009@gmail.com>
2020-06-05 01:17:00 -0400

    refactor menu system

--- main.lua
+++ main.lua
@@ -1,66 +1,20 @@
-BUTTON_HEIGHT = 64 
-WINDOW_WIDTH = love.graphics.getWidth() 
-WINDOW_HEIGHT = love.graphics.getHeight() 
-
-local 
-function newButton(text,fn)
-    return{
-        text = text, 
-        fn = fn, 
-        now = false, 
-        last = false, 
-        baseColor = {0.4,0.4,0.5, 1.0},
-        hotColor = {0.8,0.8,0.9,1.0},
-        currentColor = {}
-    }
-end 
-
-local buttons = {} 
-local font = nil  
-
 
 function love.load()
     font = love.graphics.newFont(32)
-    table.insert(buttons, newButton("Start Game", function() print("Starting Gameing") end))
-    table.insert(buttons, newButton("Load Game", function() print("Load Game") end))
-    table.insert(buttons, newButton("Settings", function() print("Setting") end))
-    table.insert(buttons, newButton("Exit", function() love.event.quit(0) end))
+    require("menu")
+
+
+
+    table.insert(menu.buttons, newButton("Start Game", function() print("Starting Gameing") end))
+    table.insert(menu.buttons, newButton("Load Game", function() print("Load Game") end))
+    table.insert(menu.buttons, newButton("Settings", function() print("Setting") end))
+    table.insert(menu.buttons, newButton("Exit", function() love.event.quit(0) end))
 end 
 
 function love.update(dt)
-
+    UpdateMenu()
 end 
 
 function love.draw()
-    local BUTTON_WIDTH = WINDOW_WIDTH * (1/3)
-    local margin = 16
-    local total_height = (BUTTON_HEIGHT + margin) * #buttons
-
-    for i, button in ipairs(buttons) do 
-        button.last = button.now 
-
-        local x = (WINDOW_WIDTH/2) - (BUTTON_WIDTH/2) 
-        local y = (WINDOW_HEIGHT/2) - (total_height/2) + ((BUTTON_HEIGHT + margin) * i)
-
-        local mx, my = love.mouse.getPosition() 
-        local hot = mx > x and mx < x + BUTTON_WIDTH and my > y and my < y + BUTTON_HEIGHT
-    
-        button.currentColor = button.baseColor
-
-        if hot then button.currentColor = button.hotColor end 
-        
-        button.now = love.mouse.isDown(1)
-
-        if button.now and not button.last and hot then button.fn() end 
-
-        love.graphics.setColor(unpack(button.currentColor))
-        love.graphics.rectangle("fill", x, y, BUTTON_WIDTH, BUTTON_HEIGHT)
-        love.graphics.setColor(0,0,0,1)
-
-        local textW = font:getWidth(button.text)
-        local textH = font:getHeight(button.text)
-        love.graphics.print(button.text, font,(WINDOW_WIDTH * 0.5) - textW * 0.5,(y + textH * 0.5 ) )
-
-     end 
-
+    DrawMenu()
 end 
--- menu.lua
+++ menu.lua
@@ -1,1 +1,55 @@
-local buttons = {} 
+menu = {} 
+menu.buttonssettings = {}
+menu.buttonssettings.HEIGHT = 64
+menu.buttonssettings.WIDTH =  love.graphics.getWidth()  * (1/3)
+menu.buttonssettings.MARGIN_TOP = 16 
+menu.buttonssettings.BASECOLOR = {0.4,0.4,0.5, 1.0}
+menu.buttonssettings.HOVERCOLOR = {0.8,0.8,0.9,1.0}
+menu.buttons = {}
+menu.total_height  = 0 
+menu.WINDOW = {}
+menu.WINDOW.HEIGHT = love.graphics.getHeight()
+menu.WINDOW.WIDTH = love.graphics.getWidth()
+
+function DrawMenu()
+    for i, button in ipairs(menu.buttons) do 
+        love.graphics.setColor(unpack(button.currentColor))
+        love.graphics.rectangle("fill", button.x, button.y, menu.buttonssettings.WIDTH, menu.buttonssettings.HEIGHT)
+        love.graphics.setColor(0,0,0,1)
+        love.graphics.print(button.text, font,(menu.WINDOW.WIDTH * 0.5) - button.textW * 0.5,(button.y + button.textH * 0.5 ) )
+     end 
+end 
+
+function UpdateMenu()
+    menu.total_height = (menu.buttonssettings.HEIGHT + menu.buttonssettings.MARGIN_TOP) * #menu.buttons
+
+    for i, button in ipairs(menu.buttons) do 
+        button.last = button.now 
+        button.x = (menu.WINDOW.WIDTH/2) - (menu.buttonssettings.WIDTH/2) 
+        button.y = (menu.WINDOW.HEIGHT/2) - (menu.total_height/2) + ((menu.buttonssettings.HEIGHT + menu.buttonssettings.MARGIN_TOP) * i)
+        button.currentColor = menu.buttonssettings.BASECOLOR
+
+        local mx, my = love.mouse.getPosition() 
+        button.isHovering = mx > button.x and mx < button.x + menu.buttonssettings.WIDTH and my > button.y and my < button.y + menu.buttonssettings.HEIGHT
+        
+        if button.isHovering then button.currentColor = menu.buttonssettings.HOVERCOLOR end 
+        
+        button.now = love.mouse.isDown(1)
+        if button.now and not button.last and button.isHovering then button.fn() end      
+     end 
+end 
+
+function newButton(text,fn)
+    return{
+        text = text, 
+        fn = fn, 
+        now = false, 
+        last = false, 
+        currentColor = {}, 
+        x = 0, 
+        y = 0, 
+        isHovering = false,
+        textW = font:getWidth(text),
+        textH = font:getHeight(text)
+    }
+end 
